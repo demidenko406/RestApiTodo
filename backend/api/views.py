36801840d3fd9799971d1bace24aa615b04dd3ea
kalacey <kalacey@yandex.by>
2021-08-22 17:10:03 +0300

    Permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,25 +1,31 @@
 from rest_framework import serializers
+from rest_framework import response
 from rest_framework.response import Response
 from .serializers import TagSerializer, TaskSerializer
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .models import Task,TaskTags    
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet,GenericViewSet
 from rest_framework.mixins import CreateModelMixin,UpdateModelMixin,DestroyModelMixin,ListModelMixin
 
 
+class UserPermission(BasePermission):
+    message = 'All actions is restricted to the author only.'
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
 
-class MainView(ModelViewSet):
+class MainView(ModelViewSet,UserPermission):
     
-    
+    permission_classes = [UserPermission]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer  = self.get_serializer(instance)
-        tags = TaskTags.objects.all()
+        tags = TaskTags.objects.all().filter(user = self.request.user)
         tags_serializer = TagSerializer(tags, many=True)
         return Response({
             'tags':tags_serializer.data,
@@ -27,9 +33,9 @@ class MainView(ModelViewSet):
             })
     
     def list(self, request, *args, **kwargs):
-        tasks = Task.objects.all()
+        tasks = Task.objects.all().filter(user = self.request.user)
         task_serializer = TaskSerializer(tasks, many=True)
-        tags = TaskTags.objects.all()
+        tags = TaskTags.objects.all().filter(user = self.request.user)
         tags_serializer = TagSerializer(tags, many=True)
         return Response({
             'tasks':task_serializer.data,
@@ -40,9 +46,14 @@ class MainView(ModelViewSet):
 
 class TagViewSet(ModelViewSet):
     
-    
+    permission_classes = [UserPermission]
     queryset = TaskTags.objects.all()
     serializer_class = TagSerializer
+    
+    def list(self, request, *args, **kwargs):
+        tags = TaskTags.objects.all().filter(user = self.request.user)
+        response = TagSerializer(tags, many=True)
+        return Response(response.data)
     
     
     def retrieve(self, request, *args, **kwargs):
@@ -51,7 +62,7 @@ class TagViewSet(ModelViewSet):
         print(serializer.data)
         tasks = Task.objects.filter(tag = instance)
         task_serializer = TaskSerializer(tasks, many=True)
-        tags = TaskTags.objects.all()
+        tags = TaskTags.objects.all().filter(user = self.request.user)
         tags_serializer = TagSerializer(tags, many=True)
         
 
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), # new
+    path('api/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
