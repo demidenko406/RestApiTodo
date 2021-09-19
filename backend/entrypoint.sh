578c5a9e90278739ff970a0d2025c22aefe6c010
kalacey <kalacey@yandex.by>
2021-09-19 04:27:50 +0300

    New

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,12 @@
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-    echo "PostgreSQL started"
-fi
+# if [ "$DATABASE" = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
+#     while ! nc -z $SQL_HOST $SQL_PORT; do
+#       sleep 0.1
+#     done
+#     echo "PostgreSQL started"
+# fi
 
-python3 manage.py migrate --no-input
+# python3 manage.py migrate --no-input
 
-exec "$@"
+# exec "$@"
