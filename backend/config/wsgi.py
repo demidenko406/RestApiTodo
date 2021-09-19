b13d69640d22b8505e470f4ec5b2f9ec4f9e5f58
kalacey <kalacey@yandex.by>
2021-09-19 04:27:41 +0300

    Minor

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+    # db:
+    #   image: postgres:12.0-alpine
+    #   volumes:
+    #     - ./postgres_data/:/var/lib/postgresql/data/
+    #   environment:
+    #     - POSTGRES_DB=todolist
+    #     - POSTGRES_USER=kalacey412
+    #     - POSTGRES_PASSWORD=kalaceydb412
